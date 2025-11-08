4a296e8c64cb54e621b7f78c1bf88fb4d67f856b
Artem Bokhonets <bohonecartem@gmail.com>
2025-11-08 21:01:40 +0700

    CSS modules and states added for add to cart and bookmark buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const sneakers = [
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/img/sneakers/1.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+  {
+    name: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "/img/sneakers/4.jpg",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,10 +40,16 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {sneakers.map((item, index) => {
+            return (
+              <Card
+                key={index}
+                price={item.price}
+                name={item.name}
+                imageUrl={item.imageUrl}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ src/components/Card/index.js
@@ -0,0 +1,44 @@
+import styles from "./Card.module.scss";
+import { useState } from "react";
+
+export default function Card({ name, price, imageUrl }) {
+  const [isAdded, setIsAdded] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const onClickFavorite = () => {
+    setIsLiked(!isLiked);
+  };
+
+  const onClickPlus = () => {
+    setIsAdded(!isAdded);
+  };
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.favorite}>
+        <img
+          onClick={onClickFavorite}
+          src={isLiked ? "/img/heart-liked.svg" : "/img/heart-unliked.svg"}
+          alt="Unliked"
+        />
+      </div>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{name}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{price} руб.</b>
+        </div>
+
+        <img
+          className={styles.imgBtnPlus}
+          onClick={onClickPlus}
+          width={30}
+          height={30}
+          src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
+          alt="Plus"
+        />
+      </div>
+    </div>
+  );
+}
